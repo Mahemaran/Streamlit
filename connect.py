150a3f0ed503925fca5accaf8e527ed93695a1ee
Mahemaran <166572866+Mahemaran@users.noreply.github.com>
2025-05-03 10:56:58 +0530

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,6 @@
+AC = {
+    "General condition":['Visual : Component Damaged', 'Visual'],
+    "Markings":["Labeling : Illegible Etch"],
+    "sticky Trigger":["Functional : Sticky Trigger", 'Trigger'],
+    "Check function of device":["Functional : Will Not Run", "Functional : Does Not Function"]
+}
